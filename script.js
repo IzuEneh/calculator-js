d24baa5c7de1c66e6b8b4d39b1e3ddbd30828822
Izu Eneh <ehen@ualberta.ca>
2022-01-26 21:38:55 -0700

    calculator works for very basic case

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const numPadSymbols = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, '.']
 const operationsContainer = document.querySelector('.operation-btns');
 const numPadContainer = document.querySelector(".num-btns");
 const screenDiv = document.querySelector('.screen');
+screenDiv.textContent = 0;
+let lastNum = null;
+let lastOperator = null;
 
 createElementsFromArrayInDiv(operationSymbols, operationsContainer, "operation-btn");
 createElementsFromArrayInDiv(numPadSymbols, numPadContainer, "num-btn");
 
 const numPadButtons = [...document.querySelectorAll('.num-btn')];
-numPadButtons.forEach(num => num.addEventListener('click', addToScreen))
+numPadButtons.forEach(num => num.addEventListener('click', addNumberToScreen))
+
+const operationButtons = [...document.querySelectorAll('.operation-btn')];
+operationButtons.forEach(op => op.addEventListener('click', operateOnNumber))
 
 function createElementsFromArrayInDiv(array, div, className) {
     for(item of array) {
@@ -19,9 +25,34 @@ function createElementsFromArrayInDiv(array, div, className) {
     }
 }
 
-function addToScreen(e) {
-    if(this.textContent === '.' && screenDiv.textContent.includes('.')) return;
-    screenDiv.textContent += this.textContent;
+function isEqualsOrClear(str){
+    return str === '=' || str === 'CE';
+}
+
+function operateOnNumber(e) {
+    // if(isEqualsOrClear(this.textContent)) return;
+    if(this.textContent === '='){
+        if(lastNum && lastOperator){
+            const result = operate(lastOperator, lastNum, +screenDiv.textContent)
+            lastNum = result;
+            lastOperator = null;
+            screenDiv.textContent = result;
+            return;
+        }
+    }
+    lastNum = +screenDiv.textContent;
+    lastOperator = this.textContent;
+    screenDiv.textContent = ''
+}
+
+function addNumberToScreen(e) {
+    screenDiv.textContent = formatContentForScreen(this.textContent);
+}
+
+function formatContentForScreen(content) {
+    if(content === '.' && screenDiv.textContent.includes('.')) return screenDiv.textContent;
+    if(screenDiv.textContent == 0) return content;
+    return screenDiv.textContent += content;
 }
 
 function add(a, b) {
